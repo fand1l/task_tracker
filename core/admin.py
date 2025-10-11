587edaacae92c7a4ba0cd8a883b011e3492a2ae9
fand1l <fand1l@outlook.com>
2025-10-11 16:23:23 +0300

    Created admin panel: configured Task and Comment models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Task, Comment
 
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "status", "priority", "deadline")
+    list_filter = ("status", "deadline", "priority")
+    search_fields = ("name",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("task", "author", "content")
+    list_filter = ("created_at",)
+    search_fields = ("content",)
+    readonly_fields = ("created_at",)

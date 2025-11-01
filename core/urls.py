5943ee07bf4551cae67b252a41d74d81a4c96186
fand1l <fand1l@outlook.com>
2025-11-01 11:25:57 +0200

    Updated urls: Added comments and some tasks urls

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,11 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="task_list"),
-    path("<int:pk>/", views.TaskDetailView.as_view(), name="task_detail"),
-    path("task_create/", views.TaskCreateView.as_view(), name="task_create")
+    path("tasks/<int:pk>/", views.TaskDetailView.as_view(), name="task_detail"),
+    path("tasks/create/", views.TaskCreateView.as_view(), name="task_create"),
+    path("tasks/<int:pk>/edit/", views.TaskUpdateView.as_view(), name="task_update"),
+    path("tasks/<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task_delete"),
+
+    path("comments/<int:pk>/edit/", views.CommentEditView.as_view(), name="comment_edit"),
+    path("comments/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
 ]
